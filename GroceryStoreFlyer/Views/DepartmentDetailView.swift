81e9948325ff934ff0c39bf6c91d1220b50adc03
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-02-05 10:16:49 -0500

    changed the app from mac to iphone, created Detail and List View

---
+++ GroceryStoreFlyer/Views/DepartmentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DepartmentDetailView.swift
+//  GroceryStoreFlyer
+//
+//  Created by Nicholas Hwang on 5/2/2025.
+//
+
+import SwiftUI
+
+struct DepartmentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DepartmentDetailView()
+}
